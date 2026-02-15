bec6ea46fb84e4c54218463f8fb47ddcf77bdcd5
Youssef Labnine <yousseflab20@gmail.com>
2026-02-15 15:14:37 +0000

    add your package navigation react router dom

--- src/screen/HomePage.tsx
+++ src/screen/HomePage.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
@@ -6,15 +7,24 @@ export default function HomePage() {
       <nav className="w-[80%] mx-auto flex justify-between items-center py-6">
         <h1 className="text-2xl font-bold tracking-wide">YOUSSEF LABNINE</h1>
         <ul className="hidden md:flex gap-8 text-gray-300 items-center">
-          <li className="hover:text-green-400 cursor-pointer transition">
-            Home
+          <li>
+            <Link to="/" className="hover:text-green-400 transition">
+              Home
+            </Link>
           </li>
-          <li className="hover:text-green-400 cursor-pointer transition">
-            About Me
+
+          <li>
+            <Link to="/AboutMe" className="hover:text-green-400 transition">
+              About Me
+            </Link>
           </li>
-          <li className="hover:text-green-400 cursor-pointer transition">
-            Projects
+
+          <li>
+            <Link to="/projects" className="hover:text-green-400 transition">
+              Projects
+            </Link>
           </li>
+
           <li>
             <button className="bg-green-500 hover:bg-green-600 transition px-5 py-2 rounded-full text-black font-semibold">
               Contact me
